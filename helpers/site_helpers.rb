fcea90300a7ba11632c68731291ba536cf0d6387
Paul Bartlett <pdbartlett@gmail.com>
2015-01-10 09:41:59 +0000

    Added placeholder top3s page and some more glyphicons

--- helpers/site_helpers.rb
+++ helpers/site_helpers.rb
@@ -1,12 +1,18 @@
 module SiteHelpers
+  FWS="\u2005" # Fixed width space (1/4 em) as glyph font has wide spaces.
+  
+  # If the suffix is present append it following a separator.
   def separated_optional(prefix, sep, opt)
     opt ? prefix + sep + opt : prefix
   end
+  
+  # Generates an item in the Bootstrap nav bar.
   def nav_item(uri, title, glyph=nil)
+    title.sub! ' ', FWS
     opts = (uri == '/' + current_page.request_path) ? {:class=>'active'} : {}
     content_tag :li, opts do
       content_tag :a, :href=>uri do
-        glyph ? tag(:span, :class=>'glyphicon glyphicon-'+glyph) + ' ' + title : title
+        glyph ? tag(:span, :class=>'glyphicon glyphicon-'+glyph) + FWS + title : title
       end
     end
   end
